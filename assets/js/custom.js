2596d7d57fbc9dee83a88b822cca3b94a2faff9c
Muhammad Hannan Ali <mdhannan.info@gmail.com>
2021-03-08 17:46:43 +0600

    done tab design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,39 +1,39 @@
- /* *******************
- * 1. Start ToggleMenu SECTION
- ******************* */
-// ---------Responsive-navbar-active-animation-----------
-
- /* *******************
- * 2. Start Contact SECTION
- ******************* */
-//comment out the iti section to see the default bootstrap error display
-
-var input = document.querySelector("#phone");
-var iti = window.intlTelInput(input, {
-  utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@16.0.3/build/js/utils.js",
-});
-
-// store the instance variable so we can access it in the console e.g. window.iti.getNumber()
-window.iti = iti;
-
-
-
-//Bootstrap test form feedback
-'use strict';
-window.addEventListener('load', function() {
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.getElementsByClassName('needs-validation');
-  // Loop over them and prevent submission
-  var validation = Array.prototype.filter.call(forms, function(form) {
-    form.addEventListener('submit', function(event) {
-
-      console.log(form.checkValidity());
-
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add('was-validated');
-    }, false);
-  });
-}, false);
+//  /* *******************
+//  * 1. Start ToggleMenu SECTION
+//  ******************* */
+// // ---------Responsive-navbar-active-animation-----------
+//
+//  /* *******************
+//  * 2. Start Contact SECTION
+//  ******************* */
+// //comment out the iti section to see the default bootstrap error display
+//
+// var input = document.querySelector("#phone");
+// var iti = window.intlTelInput(input, {
+//   utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@16.0.3/build/js/utils.js",
+// });
+//
+// // store the instance variable so we can access it in the console e.g. window.iti.getNumber()
+// window.iti = iti;
+//
+//
+//
+// //Bootstrap test form feedback
+// 'use strict';
+// window.addEventListener('load', function() {
+//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//   var forms = document.getElementsByClassName('needs-validation');
+//   // Loop over them and prevent submission
+//   var validation = Array.prototype.filter.call(forms, function(form) {
+//     form.addEventListener('submit', function(event) {
+//
+//       console.log(form.checkValidity());
+//
+//       if (form.checkValidity() === false) {
+//         event.preventDefault();
+//         event.stopPropagation();
+//       }
+//       form.classList.add('was-validated');
+//     }, false);
+//   });
+// }, false);
